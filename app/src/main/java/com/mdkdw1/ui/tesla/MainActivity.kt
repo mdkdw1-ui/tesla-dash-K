67ad7ff613feddb8ac8c1d7db207e118e92151f1
mdkdw1-ui <mdkdw1@gmail.com>
2026-07-26 16:15:25 +0900

    Update MainActivity.kt

--- app/src/main/java/com/mdkdw1/ui/tesla/MainActivity.kt
+++ app/src/main/java/com/mdkdw1/ui/tesla/MainActivity.kt
@@ -3,24 +3,19 @@ package com.mdkdw1.ui.tesla
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.mdkdw1.ui.tesla.ui.TeslaMainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val settingsManager = SecureSettingsManager(applicationContext)
+        val repository = TeslaHubRepository()
+
         setContent {
-            MaterialTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    TeslaMainScreen()
-                }
-            }
+            MainHubScreen(
+                repository = repository,
+                settingsManager = settingsManager
+            )
         }
     }
 }
